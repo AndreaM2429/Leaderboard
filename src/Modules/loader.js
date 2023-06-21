f9542aaef63a8053b6ef219f3eb6174132ff3a26
AndreaMOr <andreamanuel2207@gmail.com>
2023-06-21 11:04:22 -0600

    Modify the module to recive a value

--- src/Modules/loader.js
+++ src/Modules/loader.js
@@ -1,42 +1,11 @@
-const playerList = [
-  {
-    player: 'Oscar',
-    score: 100,
-  },
-  {
-    player: 'Francisca',
-    score: 20,
-  },
-  {
-    player: 'Arthur',
-    score: 50,
-  },
-  {
-    player: 'Andres',
-    score: 78,
-  },
-  {
-    player: 'James',
-    score: 125,
-  },
-  {
-    player: 'Karla',
-    score: 77,
-  },
-  {
-    player: 'Cinthia',
-    score: 42,
-  },
-];
-
 const tableScrores = document.querySelector('#scoreTable');
 
-const displayScores = () => {
+const displayScores = (playerList) => {
   tableScrores.innerHTML = '';
   playerList.forEach((player, index) => {
     const newRow = document.createElement('tr');
     newRow.innerHTML = `
-      <td id="player${index + 1}">${player.player}: ${player.score}</td>
+      <td id="player${index + 1}">${player.user}: ${player.score}</td>
     `;
     if (index % 2 !== 0) {
       newRow.classList.add('opacy');
